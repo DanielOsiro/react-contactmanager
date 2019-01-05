3aebe9ee1a4d833ddb9f1ca61b17d2f34dcf9fec
danielosiro <danielosiro@hotmail.com>
2019-01-05 18:28:04 -0200

    Updates

---
+++ precache-manifest.3784c041bcf7b8ef508566a35a08a5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e40d23b8b61876c686f",
+    "url": "/react-contactmanager/static/js/runtime~main.3e40d23b.js"
+  },
+  {
+    "revision": "1ebc502bcaa84c1c13b9",
+    "url": "/react-contactmanager/static/js/main.1ebc502b.chunk.js"
+  },
+  {
+    "revision": "2559d906c2d85c7fac43",
+    "url": "/react-contactmanager/static/js/1.2559d906.chunk.js"
+  },
+  {
+    "revision": "1ebc502bcaa84c1c13b9",
+    "url": "/react-contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "2559d906c2d85c7fac43",
+    "url": "/react-contactmanager/static/css/1.72352e27.chunk.css"
+  },
+  {
+    "revision": "bd49c7444f5e1db8d1478169e2144d04",
+    "url": "/react-contactmanager/index.html"
+  }
+];
